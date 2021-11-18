d7e2a9a880be98d9dc9183a6ba1719b7975d15e5
arunkumard001 <arunkumard649@gmail.com>
2021-11-18 21:01:26 +0530

    all complete

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,9 +1,47 @@
 import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:to/app/data/todo.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  late Box<Todo> todoBox;
+  List<Todo> _todos = [];
+  List<Todo> get todos => _todos;
+  addTodo(Todo todo) {
+    _todos.add(todo);
+    todoBox.add(todo);
+    update();
+  }
 
-  final count = 0.obs;
+  TodoController() {
+    todoBox = Hive.box<Todo>("todos");
+    _todos = [];
+    for (int i = 0; i < todoBox.values.length; i++) {
+      _todos.add(todoBox.getAt(i));
+    }
+  }
+
+  updateTodo(Todo oldTodo, String newDescription) {
+    int index = _todos.indexOf(oldTodo);
+    _todos[index].description = newDescription;
+    todoBox.putAt(index, _todos[index]);
+    update();
+  }
+
+  deleteTodo(Todo todo) {
+    int index = _todos.indexOf(todo);
+
+    _todos.removeWhere((element) => element.id == todo.id);
+    todoBox.delete(index);
+    update();
+  }
+
+  changestatus(Todo todo) {
+    int index = _todos.indexOf(todo);
+    _todos[index].isCompleted = !_todos[index].isCompleted;
+    todoBox.putAt(index, _todos[index]);
+    update();
+  }
+
   @override
   void onInit() {
     super.onInit();
@@ -16,5 +54,4 @@ class HomeController extends GetxController {
 
   @override
   void onClose() {}
-  void increment() => count.value++;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:to/app/data/todo.dart';
 import 'app/routes/app_pages.dart';
 
-void main() {
+Future<void> main() async {
+  Hive.registerAdapter<Todo>(TodoAdapter());
+  WidgetsFlutterBinding.ensureInitialized();
+  Directory dir = await getApplicationDocumentsDirectory();
+  await Hive.initFlutter(dir.path);
+  await Hive.openBox<Todo>("todos");
   runApp(
     GetMaterialApp(
-      title: "Application",
+      debugShowCheckedModeBanner: false,
+      title: "ToDo App",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
